23fa7df2f72af891d8f45b5da3cffbbd42f94236
Nofal71 <hafiznofal76@gmail.com>
2024-01-21 08:03:23 +0500

    Almost completed

--- Grade.cpp
+++ Grade.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
-class Grade{
- protected:
-double Total_Marks , Obtained_Marks , Percentage;
- public:
-string Student_Name;
-   Grade(){
-    Total_Marks = 0;
-    Obtained_Marks = 0;
-    
-   }
 
-double calPercentage(double Obtained_Marks , double Total_Marks){
-    Percentage = (Obtained_Marks/Total_Marks)*100;
-    return Percentage;
-}
+class Grade {
+protected:
+    double Total_Marks, Obtained_Marks, Percentage;
 
+public:
+    Grade() {
+        Total_Marks = 0;
+        Obtained_Marks = 0;
+    }
 
-string calGrade(double Percentage){
-         if (Percentage >= 90 && Percentage <= 100) {
+    double calPercentage(double Obtained_Marks, double Total_Marks) {
+        Percentage = (Obtained_Marks / Total_Marks) * 100;
+        return Percentage;
+    }
+
+    string calGrade(double Percentage) {
+        if (Percentage >= 90 && Percentage <= 100) {
             return "A+";
         } else if (Percentage >= 85 && Percentage < 90) {
             return "A";
@@ -42,96 +42,104 @@ string calGrade(double Percentage){
         } else {
             return "Failure";
         }
-}
-
- 
-
+    }
+    
 };
 
-class PerSubject : Grade {
+class PerSubject : public Grade {
 public:
+    string student;
 
-void Main(int subCount) {
-    double Total[subCount], Obtained[subCount];
-    int choice;
+    void Main(int subCount) {
+        Total_Marks = 0;
+        Obtained_Marks = 0;
+        double Total[subCount], Obtained[subCount];
+        int choice;
 
-    for (int i = 0; i < subCount; i++) {
-        cout << "\t\tSubject number " << i + 1 << endl;
-        cout << "\t\t\tObtained Marks : ";
-        cin >> Obtained[i];
-        cout << "\t\t\tOut Of  :";
-        cin >> Total[i];
+        for (int i = 0; i < subCount; i++) {
+            cout << "\n\t\t\tSubject number " << i + 1 << endl;
+            cout << "\t\t\t\tObtained Marks : ";
+            cin >> Obtained[i];
+            cout << "\t\t\t\tOut Of  :";
+            cin >> Total[i];
 
-        if (Obtained[i] > Total[i]) {
-            cout << "Invalid Marks Entry. Try again." << endl;
-            i--; 
-        } else {
-            Total_Marks += Total[i];
-            Obtained_Marks += Obtained[i];
+            if (Obtained[i] > Total[i]) {
+                cout << "Invalid Marks Entry. Try again." << endl;
+                i--;
+            } else {
+                Total_Marks += Total[i];
+                Obtained_Marks += Obtained[i];
+            }
+        }
+
+        system("CLS");
+
+        cout << "\n\n\n";
+        cout << "\tChoose Options : " << endl;
+        cout << "\t1- Total Grades " << endl;
+        cout << "\t2- Grade per Subject " << endl;
+        cout << "\t3- Marks  " << endl;
+        cout << "\t4- Result ";
+        cin >> choice;
+
+        if (choice == 1) {
+            cout << "Total Grade For " << student << " is " << endl;
+            cout << "\t\tTotal Grade: " << calGrade(calPercentage(Obtained_Marks, Total_Marks)) << endl;
+        } else if (choice == 2) {
+            cout << "Grade Per Subject For " << student << " is " << endl;
+            for (int i = 0; i < subCount; i++) {
+                cout << "\t\tSubject " << i + 1 << "  Grade : " << calGrade(calPercentage(Obtained[i], Total[i])) << endl;
+            }
+        } else if (choice == 3) {
+            cout << "\n\t\tMarks Obtained by " << student << " is " << Obtained_Marks << "/" << Total_Marks << endl;
+        } else if (choice == 4) {
+            cout << "\n\t\tMarks Obtained by " << student << " is " << Obtained_Marks << "/" << Total_Marks;
+            cout << "\n\t\tGrade Per Subject For " << student << " is " << endl;
+            for (int i = 0; i < subCount; i++) {
+                cout << "\t\tSubject " << i + 1 << "  Grade : " << calGrade(calPercentage(Obtained[i], Total[i])) << endl;
+            }
+            cout << "\t\tTotal Grade For " << student << " is " << endl;
+            cout << "\t\tTotal Grade: " << calGrade(calPercentage(Obtained_Marks, Total_Marks)) << endl;
         }
     }
-
-    cout << "\tChoose Options : " << endl;
-    cout << "\t1- Total Grades " << endl;
-    cout << "\t2- Grade per Subject " << endl;
-    cout << "\t3- Both :" << endl;
-    cin >> choice;
-
-    if (choice == 2) {
-        cout<<"Grade Per Subject For "<<Student_Name<<" is ";
-        for (int i = 0; i < subCount; i++) {
-            cout << "\t\tSubject " << i + 1 << "  Grade : " << calGrade(calPercentage(Obtained[i], Total[i])) << endl;
-        }
-    } else if (choice == 3) {
-        cout<<"Grade Per Subject For "<<Student_Name<<" is ";
-        for (int i = 0; i < subCount; i++) {
-            cout << "\t\tSubject " << i + 1 << " Grade : " << calGrade(calPercentage(Obtained[i], Total[i])) << endl;
-        }
-        cout << "\t\tTotal Grade: " << calGrade(calPercentage(Obtained_Marks, Total_Marks)) << endl;
-    } else if (choice == 1) {
-        cout<<"Total Grade For "<<Student_Name<<" is ";
-        cout << "\t\tTotal Grade: " << calGrade(calPercentage(Obtained_Marks, Total_Marks)) << endl;
-    }
-}
-
-
 };
-
 
 int main() {
-    int count ;
-    char choise;
+    int count;
+    char choice;
+
     PerSubject object;
     Grade obj;
-    cout<<"\n\n\n";
-    cout << "\t\t\t\t\t++++++++++++++++++++++++++++++++++++++" << endl;
-    cout << "\t\t\t\t\t|  WELCOME TO STUDENT GRADE TRACKER  | " << endl;
-    cout << "\t\t\t\t\t++++++++++++++++++++++++++++++++++++++" << endl;
+
+    cout << "\n\n\n";
+    cout << "\t\t++++++++++++++++++++++++++++++++++++++" << endl;
+    cout << "\t\t|  WELCOME TO STUDENT GRADE TRACKER  | " << endl;
+    cout << "\t\t++++++++++++++++++++++++++++++++++++++" << endl;
     cout << "\n\n\t\tEnter Student Name : ";
-    cin>>obj.Student_Name;
-    cout << "\n\n\t\tEnter Subject Counts You Want to calculate : ";
+    cin >> object.student;
+    cout << "\n\tEnter Subject Counts You Want to calculate : ";
     cin >> count;
 
+    object.Main(count);
 
-object.Main(count);
+    int y = 1;
+    while (y > 0) {
+        
+        cout << "Again Calculate (y/n) : ";
+        cin >> choice;
+        
+        if (choice == 'y') {
+        	system("CLS");
+            cout << "Enter Subject Counts You Want to calculate : ";
+            cin >> count;
+            object.Main(count);
+        } else {
+            break;
+        }
 
-int y = 1;
-while(y>0){
-
-
-cout<<"Again Calculate (y/n) : ";
-cin>>choise;
-if(choise == 'y'){
-    cout << "Enter Subject Counts You Want to calculate : ";
-    cin >> count;
-object.Main(count);
-
-}else{
-    break;
-}
-y++;
-}
+        y++;
+    }
 
     return 0;
-    
 }
+
